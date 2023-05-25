7c50cd17a51125323c39da0d4a966d170062659a
PriyankaSakharkar <priyanka@sakharkar>
2023-05-25 19:58:57 +0530

    one change

---
+++ src/components/Cal/Cal.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import CalWrapper from "../CalWrapper/CalWrapper";
+
+
+const Cal = () => {
+
+    const [inputVal1, setInputVal1] = useState(0);
+
+    const allBtArr = [
+        {value: 0, btFun: () => {
+            setInputVal1(0)
+        }},
+        {value: 1, btFun: () => {
+            setInputVal1(1)
+        }},
+        {value: 2, btFun: () => {
+            setInputVal1(2)
+        }},
+        {value: 3, btFun: () => {
+            setInputVal1(3)
+        }},
+        {value: 4, btFun: () => {
+            setInputVal1(4)
+        }},
+        {value: 5, btFun: () => {
+            setInputVal1(5)
+        }},
+        {value: 6, btFun: () => {
+            setInputVal1(6)
+        }},
+        {value: 7, btFun: () => {
+            setInputVal1(7)
+        }},
+        {value: 8, btFun: () => {
+            setInputVal1(8)
+        }},
+        {value: 9, btFun: () => {
+            setInputVal1(9)
+        }},
+    ];
+
+    return (
+        <div>
+            <p> {inputVal1}</p>
+            <CalWrapper  allBtArr={allBtArr}/>
+        </div>
+    )
+};
+
+export default Cal;
